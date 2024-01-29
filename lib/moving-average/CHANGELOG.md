8426fadadecce8eb2377bd047c3441d8256851cd
Gerhard Malan <gmalan@gmconsult.com.au>
2024-01-29 10:46:44 -0400

    v0.0.1

---
+++ lib/moving-average/CHANGELOG.md
@@ -0,0 +1,5 @@
+<!-- moving-average -->
+
+## 0.0.1
+
+* Initial version.
---
+++ lib/moving-average/include/EMA.h
@@ -0,0 +1,70 @@
+/*!
+* @file EMA.h
+*
+* @mainpage Exponentially Weighted Moving Average (EMA) filter.
+*
+* @section intro_sec_Introduction
+*
+* Exponentially Weighted Moving Average (EMA) filter used for smoothing data series readings.
+* 
+* @section author Author
+* 
+* Gerhard Malan for GM Consult Pty Ltd
+* 
+ * @section license License
+ * 
+ * This library is open-source under the BSD 3-Clause license and 
+ * redistribution and use in source and binary forms, with or without 
+ * modification, are permitted, provided that the license conditions are met.
+ * 
+ * EMA Filter - Exponentially Weighted Moving Average filter used for smoothing data series readings.
+ *
+ *     output = alpha * reading + (1 - alpha) * lastOutput
+ *
+ * Where:
+ *  -   alpha = factor greater than 0 and less or equal to 1
+ *  -   reading = current input value
+ *  -   lastOutput = last filter output value
+ *  -   output = filter output value after the last reading
+ *
+ */
+
+#ifndef EMA_H_
+#define EMA_H_
+
+class EMA {
+public:
+/*
+	 * Current data output
+	 */
+	double output = 0;
+	/*
+	 * Smoothing factor, in range [0,1]. Higher the value - less smoothing (higher the latest reading impact).
+	 */
+	double alpha = 0;
+		
+	/*
+	 * Creates a filter without a defined initial output. The first output will be equal to the first input.
+	 */
+	EMA(double alpha);
+
+	/*
+	 * Creates a filter with a defined initial output.
+	 */
+	EMA(double alpha, double initialOutput);
+
+	void reset();
+
+	/*
+	 * Specifies a reading value.
+	 * @returns current output
+	 */
+	double filter(double input);
+
+private:
+
+	bool hasInitial = false;
+
+};
+
+#endif /* EMA_H_ */
