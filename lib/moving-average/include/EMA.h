712ed30e688eca9336c36a9de25d0f3910a49429
Gerhard Malan <gmalan@gmconsult.com.au>
2024-01-29 11:23:44 -0400

    v0.0.1+2

--- lib/moving-average/include/EMA.h
+++ lib/moving-average/include/EMA.h
@@ -67,4 +67,30 @@ private:
 
 };
 
+
+EMA::EMA(double alpha) {
+	this->alpha = alpha;
+}
+
+EMA::EMA(double alpha, double initialOutput) {
+	this->alpha = alpha;
+	this->output = initialOutput;
+	this->hasInitial = true;
+}
+
+void EMA::reset() {
+	this->hasInitial = false;
+}
+
+double EMA::filter(double input) {
+	if (hasInitial) {
+		output = alpha * (input - output) + output;
+	} else {
+		output = input;
+		hasInitial = true;
+	}
+	return output;
+}
+
+
 #endif /* EMA_H_ */
